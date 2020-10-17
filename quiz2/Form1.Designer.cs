ddaa0e142aea1a11901fdb2f3a37d3d62cf5b2ef
playerB <skbrave1@gmail.com>
2020-10-17 15:05:34 +0700

    Add project files.

---
+++ quiz2/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace quiz2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(45, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(478, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(478, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(478, 215);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(179, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "เปลี่ยนเป็นยาฆ่าหญ้า";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(475, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ยาฆ่าแมลง";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(478, 302);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 41);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(573, 302);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 41);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "load";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "savegame1";
+            this.saveFileDialog1.Filter = "Text files|*.txt";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(612, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "time : 0 s";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ quiz2/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quiz2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rd = new Random();
+        double gameTime = 0;
+        int[,] grassBugTable = new int[8, 8];
+        int mode = 2; //1 = grass, 2 = bug
+        bool isFull = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int dx = e.X / (pictureBox1.Width / 8);
+            int dy = e.Y / (pictureBox1.Height / 8);
+            if (mode == grassBugTable[dx,dy])
+            {
+                grassBugTable[dx, dy] = 0;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            for (int i = 0 ; i <= 8 ; i++)
+            {
+                Point v1 = new Point(pictureBox1.Width * i / 8, 0);
+                Point v2 = new Point(pictureBox1.Width * i / 8, pictureBox1.Height);
+                Point h1 = new Point(0, pictureBox1.Height * i / 8);
+                Point h2 = new Point(pictureBox1.Width, pictureBox1.Height * i / 8);
+                g.DrawLine(Pens.Black, v1, v2);
+                g.DrawLine(Pens.Black, h1, h2);
+            }
+            for (int tx = 0 ; tx < 8 ; tx++)
+                for (int ty = 0; ty < 8; ty++)
+                {
+                    if (grassBugTable[tx, ty] == 1)
+                    {
+                        g.DrawString("grass", DefaultFont, Brushes.Green, pictureBox1.Width * tx / 8, pictureBox1.Height * ty / 8);
+                    }else if (grassBugTable[tx, ty] == 2)
+                    {
+                        g.DrawString("bug", DefaultFont, Brushes.Yellow, pictureBox1.Width * tx / 8, pictureBox1.Height * ty / 8);
+                    }
+                }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isFull == true)
+            {
+                timer1.Stop();
+                MessageBox.Show("ENDED");
+                
+            }
+            else gameTime += 1;
+            label2.Text = "time : " + (gameTime / 10) + " s";
+            isFull = true;
+            foreach (int i in grassBugTable)
+            {
+                if (i == 0) isFull = false;
+            }
+            if (gameTime % 5 == 0)
+            {
+                int grassOrBug = rd.Next(1, 3); //1 = grass, 2 = bug
+                bool filled = false;
+                do
+                {
+                    int tableX = rd.Next(0, 8);
+                    int tableY = rd.Next(0, 8);
+                    if (grassBugTable[tableX, tableY] == 0)
+                    {
+                        grassBugTable[tableX, tableY] = grassOrBug;
+                        filled = true;
+                    }
+                } while (filled == false);
+            }
+            Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (mode == 1)
+            {
+                mode = 2;
+                label1.Text = "ยาฆ่าแมลง";
+                label1.ForeColor = Color.Yellow;
+                button3.Text = "เปลี่ยนเป็นยาฆ่าหญ้า";
+            }
+            else if (mode == 2)
+            {
+                mode = 1;
+                label1.Text = "ยาฆ่าหญ้า";
+                label1.ForeColor = Color.Green;
+                button3.Text = "เปลี่ยนเป็นยาฆ่าแมลง";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK) return;
+            StreamWriter sw = new StreamWriter(saveFileDialog1.FileName);
+            for (int tx = 0; tx < 8; tx++)
+                for (int ty = 0; ty < 8; ty++)
+                {
+                    sw.WriteLine("{0},{1},{2}", tx, ty, grassBugTable[tx, ty]);
+                }
+            sw.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            StreamReader sr = new StreamReader(openFileDialog1.FileName);
+            grassBugTable.Initialize();
+            String line = "";
+            String[] splitline;
+            while ((line = sr.ReadLine()) != null)
+            {
+                splitline = line.Split(',');
+                int tx = Convert.ToInt32(splitline[0]);
+                int ty = Convert.ToInt32(splitline[1]);
+                int v = Convert.ToInt32(splitline[2]);
+                grassBugTable[tx, ty] = v;
+            }
+            sr.Close();
+            timer1.Start();
+        }
+    }
+}
